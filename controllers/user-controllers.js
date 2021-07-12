0185ed3f414d6a8a2943ca590880aab2e5fa58bd
Amanda Hoshor <hoshor.manda@gmail.com>
2021-07-11 23:38:07 -0600

    what is mongoose.types.objectid, if schema.types.objectid isn't the write thing?

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -45,6 +45,21 @@ const userController = {
          .catch(err => res.status(400).json(err));
     },
 
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, {
+          new: true,
+          runValidators: true,
+        })
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'Try again, no user with this ID.' });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => res.status(400).json(err));
+      },
+
     deleteUser ({ params }, res ) {
         User.findOneAndDelete({ _id: params.id })
         .then(dbUserData => {
